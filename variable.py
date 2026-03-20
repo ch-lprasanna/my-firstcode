71814cfed05b6a91b9039cd78c8c716b08762c28
ch-lprasanna <chengalasettylakshmiprasanna3@gmail.com>
2026-03-20 13:51:24 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,100 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=10
+print(a)
+10
+print(4+5)
+9
+9=0
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?9==0
+SyntaxError: invalid syntax
+4=10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+4==10
+False
+a=2
+b=4
+print(a+b)
+6
+print(a-b)
+-2
+c=3
+print(C)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    print(C)
+NameError: name 'C' is not defined. Did you mean: 'c'?
+print(c)
+3
+name="prasanna"
+print(name)
+prasanna
+print("name")
+name
+AGE=18
+print(AGE)
+18
+#i=1
+print(i)
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print(i)
+NameError: name 'i' is not defined. Did you mean: 'id'?
+if=10
+SyntaxError: invalid syntax
+esle if=10
+SyntaxError: invalid syntax
+a=2;b=3
+print(a+b)
+5
+print(A+b)
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    print(A+b)
+NameError: name 'A' is not defined. Did you mean: 'a'?
+a,b=6,7
+print(a-b)
+-1
+a,b="prasanna","ch"
+>>> print(a+b)
+prasannach
+>>> print(a+" "+b)
+prasanna ch
+>>> print(a b)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(a""b)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(a "" b)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(a " " b)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> fname="prasanna"
+>>> lname="ch"
+>>> print(fname+lname)
+prasannach
+>>> print(fname+"."+lname)
+prasanna.ch
+>>> fnamelname="prasanna"
+>>> print(fnamelname)
+prasanna
+>>> fname lname="prasanna"
+SyntaxError: invalid syntax
+>>> fname_name="prasanna"
+>>> print(fname_lname)
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    print(fname_lname)
+NameError: name 'fname_lname' is not defined. Did you mean: 'fnamelname'?
+>>> fname_lname="prasanna"
+>>> print(fname_lname)
+prasanna
+>>> a=1
+>>> print(a)
+1
+>>> del a
+>>> a
+Traceback (most recent call last):
+  File "<pyshell#49>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
